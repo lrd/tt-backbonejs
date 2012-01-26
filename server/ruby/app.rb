660385092656f2fe3ee836f10721b4e9611c06ea
Dave Clark <me@clarkdave.net>
2012-01-26 12:42:30 +0000

    removed old memcache stuff

--- server/ruby/app.rb
+++ server/ruby/app.rb
@@ -1,7 +1,6 @@
 require 'rubygems'
 require 'sinatra'
 require 'json'
-require 'memcache'
 require 'active_record'
 
 ActiveRecord::Base.establish_connection(
@@ -16,22 +15,6 @@ end
 
 puts Project.all.to_json
 
-# memcached -p 11211 &
-# curl -v -d @d localhost:4567
-# where d is a file :
-# {
-# "id": 6,
-# "name": "Take over the world",
-# "effort": 5,
-# "people": 5000,
-# "reason": "Bored on a Sunday afternoon"
-# }
-# 
-# curl -v -X DELETE localhost:4567/6
-# curl -v localhost:4567/6
-# curl -v localhost:4567/
-cache = MemCache.new(["localhost:11211"])
-
 get '/' do
   File.read File.join('public', 'basic/index.html')
 end
@@ -39,24 +22,6 @@ end
 get "/projects" do
   content_type :json
   Project.all.to_json
-end
-
-get "/projects/:id" do
-  content_type :json
-  proj = cache.get( 'projects' ).find{ |i| i["id"] == params[:id].to_i }
-  if proj.nil?
-    404
-  else
-    proj.to_json
-  end
-end
-
-delete "/projects/:id" do
-  content_type :json
-  proj = cache.get('projects')
-  proj.delete_if{ |i| i["id"] == params[:id].to_i }
-  cache.set( 'projects', proj )
-  200
 end
 
 put "/projects/:id/?" do
